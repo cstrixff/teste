8995419528ddd9c19ba4dd98657ce73f3f36c6f1
cstrix <cstrix@gmail.com>
2020-11-21 22:36:17 +0000

    commit inicial

---
+++ App8/App8/Dados.cs
@@ -0,0 +1,107 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Xamarin.Forms;
+
+namespace App8
+{
+    static class Dados
+    {
+        static public string conString = "server=cstrix.com; database=esffpap_exemplo; uid=esffpap_mocho; pwd=filped13;";
+        static public MySqlConnection conn = null;
+  
+        static private void AbrirLigacao()
+        {
+           // try
+           // {
+                conn = new MySqlConnection(conString);
+                conn.Open();
+               // Toast.MakeText(Application.Context, "Ligação estabelecida com sucesso", ToastLength.Long).Show();
+               
+           // }
+           // catch (Exception ex)
+           // {
+
+               // Toast.MakeText(Application.Context, "Erro na ligação" + ex, ToastLength.Long).Show();
+            //}
+        }
+
+        static public List<Jogo> Jogos()
+        {
+            AbrirLigacao();
+
+            List<Jogo> listaJogos = new List<Jogo>();
+
+            var querysql = "SELECT * FROM Jogo";
+
+            MySqlDataReader leitor = null;
+            MySqlCommand cmd = new MySqlCommand(querysql, conn);
+            
+            leitor = cmd.ExecuteReader();
+            
+           // MessageBox.Show(leitor.FieldCount + " z"+ leitor.HasRows.ToString() + " "+cmd.ToString());
+            while (leitor.Read())
+            {
+                //MessageBox.Show("as");
+                Jogo jogo = new Jogo(leitor["ID"].ToString(), leitor["titulo"].ToString(), leitor["preco"].ToString(), leitor["capa"].ToString());
+                //Jogo jogo = new Jogo(12, "12", 12);
+
+                listaJogos.Add(jogo);
+
+
+            }
+            FecharLigacao();
+            return listaJogos;
+
+        }
+
+        static private void FecharLigacao()
+        {
+            try
+            {
+               
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                //Toast.MakeText(Application.Context, "Erro na ligação" + ex, ToastLength.Long).Show();
+            }
+        }
+        /*
+        static public Utilizador Login(string username, string password)
+        {
+            AbrirLigacao();
+            Utilizador utilizador;
+
+            var querysql = "SELECT * FROM utilizador WHERE username=@user AND password = @pass";
+
+            MySqlDataReader leitor = null;
+            MySqlCommand cmd = new MySqlCommand(querysql, conn);
+            cmd.Parameters.AddWithValue("@user", username);
+            cmd.Parameters.AddWithValue("@pass", password);
+            leitor = cmd.ExecuteReader();
+
+            //MessageBox.Show(leitor.FieldCount + " z"+ leitor.HasRows.ToString() + " "+cmd.ToString());
+            if (leitor.HasRows)
+            {
+                leitor.Read();
+                utilizador = new Utilizador(Convert.ToInt32(leitor["ID"]), leitor["nome"].ToString(), leitor["username"].ToString(), leitor["password"].ToString(), Convert.ToInt32(leitor["tipo"]));
+                FecharLigacao();
+                return utilizador;
+                                  
+            }
+            FecharLigacao();
+            return null;
+
+
+
+
+        }*/
+    }
+}
---
+++ App8/App8/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            colecaoJogos.ItemsSource = Dados.Jogos();
+            
+        }
+
+        private void colecaoJogos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //PageDetalhes nova = (Jogo)colecaoJogos.SelectedItem)
+            Navigation.PushAsync(new PageDetalhes((Jogo)colecaoJogos.SelectedItem));
+            
+        }
+    }
+}
